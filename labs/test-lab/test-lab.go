b62cfbf7632a096609ce28aedafe566c7fd02149
Alejandra Velazquez <judithvelram@gmail.com>
2021-02-08 03:45:48 +0000

    Homework done: test-lab
    Signed-off-by: Alejandra Velazquez <judithvelram@gmail.com>

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+	if len(os.Args) > 1{
+		message := ""
+		for i := 1; i < len(os.Args); i++{
+			message += os.Args[i]
+			if i != len(os.Args)-1{
+				message += " "
+			}
+		}
+		fmt.Println("Hello "+ message +". Welcome to the jungle")
+	}else{
+		fmt.Println("No arguments received")
+	}
+	
 }
